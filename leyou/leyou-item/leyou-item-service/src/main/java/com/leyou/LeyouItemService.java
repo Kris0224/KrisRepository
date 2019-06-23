ff6589ac82f483861b687ecd83a487753d68a5a8
Kris <sanyu0224@163.com>
2019-06-23 21:36:03 +0800

    乐优商城1.2.0 1.跨域问题的处理 2.nginx服务器反向代理的实现 3.分类管理模块功能的实现(前端+后端+数据库) 4.品牌管理前端页面的实现 5.品牌管理后端微服务架构的搭建

--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemService.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemService.java
@@ -1,6 +1,7 @@
 package com.leyou;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.mapper")//添加mapper的扫描
 public class LeyouItemService {
     public static void main(String[] args) {
         SpringApplication.run(LeyouItemService.class, args);
