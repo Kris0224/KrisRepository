b6012d0df582289aa93fbb0f597315a1b0c4a2d2
Kris <sanyu0224@163.com>
2019-06-25 21:11:37 +0800

    乐优商城1.3.0 1.实现品牌列表查询关键字排序以及展示 2.实现品牌的新增业务 3.实现上传微服务的构建和后端实现

--- leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemService.java
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemService.java
@@ -1,17 +1,19 @@
 package com.leyou;
 
 
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * service微服务启动类
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("com.leyou.mapper")//添加mapper的扫描
+//@MapperScan("com.leyou.mapper")//添加springboot的mapper的扫描
+@MapperScan("com.leyou.mapper")//添加通用mapper的mapper的扫描
 public class LeyouItemService {
     public static void main(String[] args) {
         SpringApplication.run(LeyouItemService.class, args);
